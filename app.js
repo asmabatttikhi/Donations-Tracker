22f69f38026c627f5cc55e8f04ab5c4d32cd2dc6
asmabatttikhi <asmaaalbattikhi@gmail.com>
2021-08-11 12:29:47 +0300

    exam

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// function Person (name, amount,age) {
+//   this.name = name;
+//   this.amount = amount;
+//   this.age = age;
+// }
+
+// let namedonate = new Person("ahmad", "100", "51");
+
+// get
+
+
+
+
+
+// <!--this bellow is taken from for time https://stackoverflow.com/questions/19698769/add-input-from-a-form-to-a-table-in-the-same-page-without-reloading -->
+
+// var qtyTotal = 0;
+// var priceTotal = 0;
+
+// function updateForm() {
+//   var product = document.getElementById('product').value;
+
+//   var qty = document.getElementById('quantity').value;
+//   qtyTotal = qtyTotal + parseInt(qty);
+//   document.getElementById('qtyTotals').innerHTML=qtyTotal;
+
+//   var price = document.getElementById('price').value;
+//   priceTotal = priceTotal + parseInt(price);
+//   document.getElementById('priceTotals').innerHTML=priceTotal;
+
+//   var table=document.getElementById('results');
+//   var row=table.insertRow(-1);
+//   var cell1=row.insertCell(0);
+//   var cell2=row.insertCell(1);
+//   var cell3=row.insertCell(2);
+//   cell1.innerHTML=product;
+//   cell2.innerHTML=qty;
+//   cell3.innerHTML=price;
+// }
